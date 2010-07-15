3599b8e1db4f6f9ed754c0ecff11e699aae93d66
Jesper Kristensen <jeskr@aarhus.dk>
2010-07-15 13:56:07 +0200

    Upgrade drupal core

--- sites/all/themes/vigirbyenpuls/template.php
+++ sites/all/themes/vigirbyenpuls/template.php
@@ -20,34 +20,11 @@ function vigirbyenpuls_preprocess_page(& $variables, $hook) {
 	$variables['iestyles'] .= '<link type="text/css" rel="stylesheet" media="all" href="/' . drupal_get_path(theme, vigirbyenpuls) . '/styles/ie/msie6.css" />';
 	$variables['iestyles'] .= '<![endif]-->';
 
-	// Make sure framework styles are placed above all others.
-	// $variables['css_alt'] = vigirbyenpuls_css_reorder($variables['css']);
-	// $variables['styles'] = drupal_get_css($variables['css_alt']);
 
-	//	
-	// Add taxonomy-term to body class. 
-	// Also for selected uris generated additional template suggestions.
-	//
+  // Generate header_graphic variable for printing
+  	if (module_exists('path')) {
 
-	// Confirm module path enabled	
-	if (module_exists('path')) {
-
-		// Fix header image
-		/*
-		$node= $variables['node'];
-		if (isset ($node->field_header[0])) 
-        { // Node field header exists
-            if ($node->field_header[0][nid] != null)
-            { // And is liked to a valid node
-				$node= node_load($node->field_header[0][nid]);
-				$variables[header_graphic]= '<div id="header-graphic">' .
-											' <img src="/' . $node->field_graphic[0][filepath] . '">' .
-											' <div class="caption"><span class="span">' . $node->title . '</span></div>' .
-											'</div>';
-			}
-		}
-		*/
-		$node= $variables['node'];
+		$node = $variables['node'];
 		if (isset ($node->field_header[0])) 
         { // Node field header exists
             if ($node->field_header[0][nid] != null)
@@ -56,12 +33,20 @@ function vigirbyenpuls_preprocess_page(& $variables, $hook) {
 				$variables[header_graphic]= '<img id="header-graphic" src="/' . $node->field_graphic[0][filepath] . '" />';
 			}
 		}
+  }
+
+  //
+  // Generate additional template suggestions for page.tpl.php
+  //
+  
+	// Confirm module path enabled	
+	if (module_exists('path')) {
 
 		// Hent url alias og isoler sidste del af urlen
-		$url_alias= drupal_get_path_alias($_GET['q']);
-		$alias_parts= explode('/', $url_alias);
-		$last= array_reverse($alias_parts);
-		$last_part= $last[0];
+		$url_alias = drupal_get_path_alias($_GET['q']);
+		$alias_parts = explode('/', $url_alias);
+		$last = array_reverse($alias_parts);
+		$last_part = $last[0]; 
 
 		// Tjek, at der ikke er tale om en edit-side
 		if ($last_part != "edit") {
@@ -69,40 +54,40 @@ function vigirbyenpuls_preprocess_page(& $variables, $hook) {
 			// Check if single node is being displayed
 			if (arg(0) == "node" && is_numeric(arg(1))) {
 
-				// Check if node assigned category, and retrive category if so
+				// Check if node is assigned taxonomy term and retrive term if so
 				if (isset ($variables['node']->taxonomy)) {
-					$target_tax= 0;
-					$node_tax= $variables['node']->taxonomy;
+					$target_tax = 0;
+					$node_tax = $variables['node']->taxonomy;
 
 					// retrieve name of category
-					$tid= array_keys($node_tax);
-					$name= $node_tax[$tid[$target_tax]]->name;
+					$tid = array_keys($node_tax);
+					$name = $node_tax[$tid[$target_tax]]->name;
 
 					// Clean output
-					$clean_name= check_plain($name);
-					$dash_name= str_replace(" ", "-", $clean_name);
-					$lc_name= strtolower($dash_name);
+					$clean_name = check_plain($name);
+					$dash_name = str_replace(" ", "-", $clean_name);
+					$lc_name = strtolower($dash_name);
 
 					// add taxonomy term to body class
-
 					$variables['body_classes'] .= $variables['body_classes'] . " tax-$lc_name";
 
 				} // end of taxonomy check
 			} // end of node/id check
 
 			// Create array for template suggestions
-			$templates= array ();
-			$template_name= "page";
+			$templates = array ();
+			$template_name = "page";
 
 			// Add url-segments to template suggestions
 			foreach ($alias_parts as $part) {
 				if ($part != "page") {
-					$template_name= $template_name . '-' . $part;
-					$templates[]= $template_name;
+					$template_name = $template_name . '-' . $part;
+					$templates[] = $template_name;
 				}
 			} // end of foreach loop
 
-			$variables['template_files']= $templates;
+			$variables['template_files'] = $templates;
+		
 
 		} // end of edit check
 	} // end of if module exists
